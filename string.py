7fef6de7e28cc5c35564904e44c631a7dbc9ecc3
komi53 <kxs53kuro@gmail.com>
2021-11-07 22:41:08 +0900

    string_method

--- string.py
+++ string.py
@@ -21,3 +21,17 @@ print('Py''thon')
 
 prefix = 'Py'
 print(prefix + 'thon')
+
+word = 'python'
+print(word[0])
+print(word[-1])
+print(word[0:5])
+print(word[2:5])
+print(word[:5])
+print(word[2:])
+print('#########')
+word = 'j' + word[1:]
+print(word)
+print(word[:])
+n = len(word)
+print(n)
