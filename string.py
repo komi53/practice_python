fbf782491a5f6a08ba94dfaeb6779e415eafcd42
komi53 <kxs53kuro@gmail.com>
2021-11-07 22:12:08 +0900

    python start

---
+++ string.py
@@ -0,0 +1,23 @@
+print('hello')
+print("I don't know")
+print('I don\'t know')
+print("I don't know")
+
+print('hello,\nHow are you?')
+
+print(r'C:\name\name')
+
+print("#########")
+print("""\
+line1
+line2
+line3\
+""")
+print("#########")
+
+print('Hi,' * 3 + 'Mike.')
+
+print('Py''thon')
+
+prefix = 'Py'
+print(prefix + 'thon')
